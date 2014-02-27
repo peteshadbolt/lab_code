1c6660158808854f08b6040065dd291ad8f9ad81
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-02-27 15:16:38 +0000

    server is getting there

--- dpc230_counting/client_example.py
+++ dpc230_counting/client_example.py
@@ -1,33 +1,35 @@
 import socket
+import sys
 
 class client:
     def __init__(self):
         ''' An example client for the DPC230 server '''
         self.connect()
 
-
     def connect(self):
         serverHost = 'localhost'
-        serverPort = 50007
-        print 'Trying to connect to server on port %d ...' % serverPort, 
+        serverPort = 9999
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((serverHost, serverPort))
-        print 'Done'
-
+        try:
+            self.sock.connect((serverHost, serverPort))
+        except socket.error:
+            print 'Make sure that the DPC230 server is running!'
+            sys.exit(0)
 
     def kill(self):
-        print 'Closing client...'
         self.sock.close()    
 
-
     def say(self, message):
-        print 'Sending %s' % message
         self.sock.sendall(message)
         response = self.sock.recv(65536)
         print 'Got %s back from the server' % response
 
 if __name__=='__main__':
-    c=client()
     while True:
         message=raw_input(' > ')
-        c.say(message)
+        c=client()
+        c.say(message+'\n')
+        c.kill()
+
+
+    

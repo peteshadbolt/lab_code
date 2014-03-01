87ca1d7644cbd85532f4ba3fdc58dbc658254aa4
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-03-01 23:39:35 +0000

    start on postprocessing

--- dpc230_counting/server.py
+++ dpc230_counting/server.py
@@ -2,8 +2,8 @@ import socket
 import sys
 from multiprocessing import Process, Pipe
 import time
-from qy.hardware import dpc230
 import qy.settings
+from easy_dpc import easy_dpc
 import msvcrt
 
 
@@ -12,32 +12,6 @@ def postprocessing_loop(pipe):
 	s=postprocessing_server(pipe)
 	s.mainloop()
 
-
-class dpc_easy:
-    ''' Talk to the DPC230 with less overhead '''
-    def __init__(self, callback):
-        ''' Set up defaults '''
-        self.integration_time=1
-        self.photon_buffer_switch=0
-        self.photon_buffer_1=qy.settings.get('photon_buffer_1')
-        self.photon_buffer_2=qy.settings.get('photon_buffer_2')
-        self.dpc230=dpc230.dpc_daq(callback=callback)
-        self.dpc230.print_setup_summary()
-
-
-    def count_once(self):
-        ''' The machine counts photons for one integration time '''
-        # Choose a buffer
-        which_buffer=self.photon_buffer_2 if self.photon_buffer_switch else self.photon_buffer_1
-        self.photon_buffer_switch = not self.photon_buffer_switch
-        tdc1, tdc2 = self.dpc230.count(which_buffer, self.integration_time)
-        return tdc1, tdc2
-
-
-    def kill(self):
-        ''' Close connection '''
-        self.dpc230.kill()
-    
 
 class dpc230_counting_server:        
     def __init__(self):
@@ -79,7 +53,7 @@ class dpc230_counting_server:
         while not msvcrt.kbhit():
             self.connection, self.client_address = self.sock.accept()
             print 'Connected to client %s, %s' % self.client_address
-            self.dpc=dpc_easy(callback=self.dpc_callback)
+            self.dpc=easy_dpc(callback=self.dpc_callback)
             self.session_loop()
             print 'Lost connection with %s, %s' % self.client_address
             self.connection.close()

898a80bcc603b2e438d1764ad724660c945a8a4d
Pete Shadbolt <pete.shadbolt@gmail.com>
2014-03-19 14:44:30 +0000

    For basement

--- dpc230_counting/test_photon_elf.py
+++ dpc230_counting/test_photon_elf.py
@@ -3,9 +3,10 @@ import sys
 
 if __name__=='__main__':
     photon_elf=photon_elf.threaded_photon_elf()
-    for i in range(100):
+    for i in range(1000):
         photon_elf.send('status', 'Test'+'!'*i)
         out=photon_elf.recv()
         if out:
             if out[0]=='gui_quit': sys.exit(0)
-        photon_elf.send('count_rates', {'count_rates':{'a':2004+i, 'afg':1337}})
+        rates={'a':2004+i, 'ab':1337, 'b':i, 'abc':100+i, 'ef':5}
+        photon_elf.send('count_rates', {'count_rates':rates})
